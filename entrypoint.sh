93fca4d04a7a33959511fc7395289e518f811cb0
vladimirmac <vladimirbodrin89@gmail.com>
2024-04-18 21:29:38 +0400

    fix docker-compoer

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,9 @@ DJANGO_SETTINGS_MODULE=config.settings.prod python3 manage.py migrate --noinput 
 #
 DJANGO_SETTINGS_MODULE=config.settings.prod python3 manage.py collectstatic --noinput || exit 1
 
+
+DJANGO_SETTINGS_MODULE=config.settings.prod python3 manage.py runserver 0.0.0.0:8002 || exit 1
+
 DJANGO_SETTINGS_MODULE=config.settings.prod python3 manage.py start_kafka_consumer || exit 1
 
 
